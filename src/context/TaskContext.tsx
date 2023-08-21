5038fcd65910e82b90f19af868fbbe4fe019f41c
reactlabs-dev <matt@reactlabs.io>
2023-08-21 16:16:27 -0600

    add context provider

---
+++ src/context/TaskContext.tsx
@@ -0,0 +1,44 @@
+import React, { createContext, useContext, useReducer, ReactNode } from 'react';
+
+type ActionType = {
+    type: 'ADD_TASK' | 'DELETE_TASK';
+    payload: string;
+}
+
+type StateType = string[];
+
+const TaskReducer = (state: StateType, action: ActionType ) => {
+    switch(action.type) {
+        case 'ADD_TASK':
+            return [...state, action.payload];
+        case 'DELETE_TASK':
+            return state.filter(task => task !== action.payload);
+        default:
+            return state;
+    }
+}
+
+const TaskContext = createContext<any | undefined>(undefined);
+
+interface TaskProvderProps {
+    children: React.ReactNode
+}
+
+export const TaskProvider: React.FC<TaskProvderProps> = ({ children }) => {
+    const [state, dispatch] = useReducer(TaskReducer, []);
+
+    return (
+        <TaskContext.Provider value={{ state, dispatch }}>
+            {children}
+        </TaskContext.Provider>
+    )
+}
+
+export const useTasks = () => {
+    const context = useContext(TaskContext);
+    if (!context) {
+        throw new Error('useTasks must be used within a TaskProvider');
+    }
+
+    return context;
+}
