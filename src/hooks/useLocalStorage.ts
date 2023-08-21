f91e03ecc21972fcdf29cf8253d12ce47e11df43
reactlabs-dev <matt@reactlabs.io>
2023-08-21 16:38:57 -0600

    add useLocalStorage hook

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+function useLocalStorage<T>(key: string, initialValue: T) {
+
+    // get from local storage then
+    // parse and set to state
+    const storedValue = localStorage.getItem(key);
+    const initial = storedValue ? JSON.parse(storedValue) : initialValue;    
+
+    const [value, setValue] = useState<T>(initial);
+
+    // return a wrapped version of useState's setter function
+    // that persists the new value to localStorage
+    
+    const setStoredValue = (valueToStore: T) => {
+        setValue(valueToStore);
+        localStorage.setItem(key, JSON.stringify(valueToStore));
+    }
+
+    return [value, setStoredValue];
+}
+
+export default useLocalStorage;
